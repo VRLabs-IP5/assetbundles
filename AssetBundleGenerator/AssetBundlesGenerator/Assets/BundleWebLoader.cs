425407dd98685d8f984493c42e934431ad735952
Wanyudo <andreas.leu@hotmail.com>
2022-05-11 12:01:57 +0200

    asset

--- AssetBundleGenerator/AssetBundlesGenerator/Assets/BundleWebLoader.cs
+++ AssetBundleGenerator/AssetBundlesGenerator/Assets/BundleWebLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BundleWebLoader : MonoBehaviour
 {
-    public bundleURL = ""
+    // public bundleURL = ""
     // Start is called before the first frame update
     void Start()
     {
